8e5b203ecabdf4e7e1badf38574bb2bd238333f3
Geoff Kelly <65321097+GeoffKellyNC@users.noreply.github.com>
2022-08-28 16:44:02 -0400

    updated readme

--- src/components/iframe/TwitchChat.js
+++ src/components/iframe/TwitchChat.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const TwitchChat = ({ target }) => {
+  console.log('TwitchChat', target)
   return (
     <div>
-        <iframe src={`https://www.twitch.tv/embed/${target}/chat?parent=https://berrytwitchbot.netlify.app/dashboard`}
+        <iframe src={`https://www.twitch.tv/embed/${target}/chat?parent=https://berrytwitchbot.netlify.app`}
         height="490"
         width="500"
         title='twitch-chat-iframe'
